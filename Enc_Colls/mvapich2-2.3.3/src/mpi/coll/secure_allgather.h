dbea4fa0410bd46371abc01c75b69a6bdb4a0819
Abu Naser <an16e@my.fsu.edu>
2021-04-09 22:17:15 -0400

    .

---
+++ Enc_Colls/mvapich2-2.3.3/src/mpi/coll/secure_allgather.h
@@ -0,0 +1,22 @@
+#ifndef _SECURE_ALLGATHER_
+#define _SECURE_ALLGATHER_
+
+
+#include <openssl/evp.h>
+#include <openssl/aes.h>
+#include <openssl/err.h>
+#include <openssl/aead.h>
+unsigned char ciphertext_sendbuf[(1024*1024+32)*256];
+unsigned char ciphertext_recvbuf[(1024*1024+32)*1024];
+
+
+unsigned char ciphertext[4194304+18];
+//EVP_AEAD_CTX *ctx = NULL;
+//unsigned char key [32] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f'};
+//unsigned char nonce[12] = {'1','2','3','4','5','6','7','8','9','0','1','2'};  
+int nonceCounter; 
+
+
+
+
+#endif
